510177f61cd0eb6d9ce18177aa603a4642ff7a64
Tan Sia How <tansiahow1996@gmail.com>
2025-01-04 23:42:15 +0800

    enhance theme customization in App.vue with light theme and overrides

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
 import { NConfigProvider } from 'naive-ui';
+import { lightTheme, type GlobalThemeOverrides } from 'naive-ui'
+
+const themeOverrides: GlobalThemeOverrides = {
+  common: {
+    borderRadius: '8px',
+  },
+  Table: {
+    thColor: '#F3F3F3',
+  }
+}
+
 
 </script>
 
 <template>
-  <NConfigProvider>
+  <NConfigProvider :theme="lightTheme" :theme-overrides="themeOverrides">
     <RouterView></RouterView>
   </NConfigProvider>
 </template>
