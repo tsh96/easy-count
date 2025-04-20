0852cda5a729c839202d83fc631b486b27dc17a6
Tan Sia How <tansiahow1996@gmail.com>
2025-04-20 16:52:35 +0800

    refactor: simplify filtering logic in AutoComplete component by removing Fuse.js integration

--- src/components/AutoComplete.vue
+++ src/components/AutoComplete.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { ref, toRefs, computed, watch } from 'vue';
 import { NAutoComplete } from 'naive-ui';
-import Fuse from 'fuse.js';
 
 interface Props {
   modelValue: string;
@@ -16,20 +15,13 @@ const emit = defineEmits<{
 
 const searchQuery = ref(modelValue.value);
 
-const fuse = new Fuse<string>(options.value, {
-  keys: ['label'],
-  threshold: 0.6,
-  shouldSort: true,
-  isCaseSensitive: false,
-  useExtendedSearch: true,
-})
-
-watch(options, (newValue) => {
-  fuse.setCollection(newValue);
-});
-
 const filteredOptions = computed(() => {
-  return fuse.search(searchQuery.value).map(({ item }) => item);
+  if (searchQuery.value === '') {
+    return options.value;
+  }
+  return options.value.filter((option) => {
+    return option.toLowerCase().includes(searchQuery.value.toLowerCase());
+  });
 });
 
 watch(searchQuery, (newValue) => {
